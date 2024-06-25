8437955ddf755c41dda7fc7a6cf39e5d18ffe181
Kevin <kevin@exafunction.com>
2024-06-25 10:55:21 -0700

    everything to 1.8.70

--- src/common.ts
+++ src/common.ts
@@ -13,7 +13,7 @@ import {
 } from '../proto/exa/language_server_pb/language_server_pb';
 
 const EXTENSION_NAME = 'chrome';
-const EXTENSION_VERSION = '1.8.71';
+const EXTENSION_VERSION = '1.8.70';
 
 export const CODEIUM_DEBUG = false;
 
